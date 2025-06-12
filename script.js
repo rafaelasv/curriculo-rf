0a51318df40688defa7e4c2ae6c42382aa509ff4
rafaela <rafaelajtv@outlook.com>
2025-06-11 23:24:06 -0300

    nav update

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-const items = document.querySelectorAll('.pill-item');
 
-items.forEach(item => {
-  item.addEventListener('click', () => {
-    items.forEach(i => i.classList.remove('active'));
-    item.classList.add('active');
+  const items = document.querySelectorAll('.pill-item');
+  items.forEach(item => {
+    item.addEventListener('click', () => {
+      items.forEach(i => i.classList.remove('active'));
+      item.classList.add('active');
+    });
   });
-});
+
+  window.addEventListener("scroll", function () {
+    const nav = document.querySelector("nav");
+    if (window.scrollY > 10) {
+      nav.classList.add("scrolled");
+    } else {
+      nav.classList.remove("scrolled");
+    }
+  });
